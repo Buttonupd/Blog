de316c24f94ff3a732a2a2b0c411c28be9c4cafa
Buttonupd <Dankariuki0101@gmail.com>
2020-05-06 13:03:33 +0300

    Configured the config file to handle database url

--- config.py
+++ config.py
@@ -16,6 +16,40 @@ class Config:
     FLASK_SLOW_DB_QUERY_TIME = 0.05
     FLASK_COMMENTS_PER_PAGE = 20
     FLASK_FOLLOWERS_PAGE = 20
+    FLASK_ADMIN = os.environ.get('FLASKY_ADMIN')
+
+class DevConfig (Config):
+    DEBUG= True
+    SQLALCHEMY_DATABASE_URI = ''
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = ''
+
+    @classmethod
+    def __init__(cls, app):
+        Config.init_app(app)
+        import logging
+        from logging.handlers import SMTPHandler
+        credits = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None ) is not None:
+            credits = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIN_USE_TLS', None):
+                secure = ()
+        
+
+        mail_handler = SMTPHandler(
+          host = (cls.MAIL_SERVER, cls.MAIL_PORT),
+          from = cls.MAIL_SENDER
+          to = cls.FLASK_ADMIN
+          subject = cls.SUBJECT_PREFIX
+          credits = credits
+          secure = secure)
+          mail_handler.setLevel(logging.ERROR)
+          app.logger.addHandler = (mail_handler)
+          
+
+        
 
 
-class DevConfig 
+
