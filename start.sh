79e73d0f4af41272fb0fca9354ba4d95db34836f
Buttonupd <Dankariuki0101@gmail.com>
2020-05-09 13:41:15 +0300

    some for unknown reason, the confirmation link seems not to work

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-MAIL_USERNAME = 'blogbuttonup@gmail.com'
-MAIL_PASSWORD= 'znteeiyactwnrlik'
-SECRET_KEY = 'LET IT BEGIN'
+MAIL_USERNAME='blogbuttonup@gmail.com'
+MAIL_PASSWORD='znteeiyactwnrlik'
+SECRET_KEY='LETITBEGIN'
+SECURITY_PASSWORD_SALT='nULLNONE'
+python3.7 manage.py runserver
