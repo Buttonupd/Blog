4bb3876fdff56a600d795257ad9fcca458148ce0
Buttonupd <Dankariuki0101@gmail.com>
2020-05-09 16:54:50 +0300

    changed model file to initialize a confirmation link once a user register

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from markdown import markdown
 import bleach
 from flask import current_app, request, url_for
 from flask_login import UserMixin, AnonymousUserMixin
-from wtforms import ValidationError
+from app.exceptions import ValidationError
 from . import db, login_manager
 
 class Permission:
