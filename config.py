f05d715cd1b7f05340e03572c4f63c97216f1d80
Buttonupd <Dankariuki0101@gmail.com>
2020-05-09 22:35:53 +0300

    stable

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SSL_DISABLE = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
