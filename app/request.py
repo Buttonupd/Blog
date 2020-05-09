c60683ff39b6b5a60fb5eb8d29d1853a0199e4a5
Buttonupd <Dankariuki0101@gmail.com>
2020-05-09 17:37:42 +0300

    change the design to meet standards as set

---
+++ app/request.py
@@ -0,0 +1,11 @@
+import request
+import json
+
+def get():
+    response = request.ge('http://quotes.stormconsultancy.co.uk/random.json')
+    if response.status==200:
+        quote = response.json()
+
+        return qoute
+
+        
