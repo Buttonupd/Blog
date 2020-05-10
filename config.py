ebf5f7cfdb29ddf04e2b1339279149fd2783e3d0
Buttonupd <Dankariuki0101@gmail.com>
2020-05-10 14:26:11 +0300

    sya

--- config.py
+++ config.py
@@ -94,6 +94,6 @@ config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
-    'heroku': HerokuConfig,
+    # 'heroku': HerokuConfig,
     'default': DevelopmentConfig
 }
